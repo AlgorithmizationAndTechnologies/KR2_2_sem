2f134ab3c45521ebca6cb9ea52fe849be9232c89
M1roll <gludara104@gmail.com>
2025-05-31 19:30:14 +1000

    Update 3 Task.cpp

--- 3 Task.cpp
+++ 3 Task.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 string Bit_stat(unsigned short x, unsigned int n) {
     unsigned short mask = 1 << n - 1;
-    if ((x & mask) > 0 == 0)
+    if ((x & mask) == 0)
         return "Off";
     else
         return "On";
 }
-void PrintBin(unsigned short X) {
+void Print_bin(unsigned short x) {
     for (int i = sizeof(unsigned short) * 8 - 1; i >= 0; i--) {
-        cout << (X >> i & 1);
+        cout << (x >> i & 1);
     }
     cout << endl;
 }
@@ -19,5 +19,5 @@ int main()
     unsigned short x = 10;
     unsigned short n = 2;
     cout << "Bit status: " << Bit_stat(x, n) << endl;
-    PrintBin(x);
+    Print_bin(x);
 }
