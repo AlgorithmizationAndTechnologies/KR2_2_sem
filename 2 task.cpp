11c172776feafe99f391d4d31f91e08deeeaca7b
M1roll <gludara104@gmail.com>
2025-05-31 19:45:44 +1000

    Update 2 task.cpp

--- 2 task.cpp
+++ 2 task.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+double Res(double x, double n) {
+    return pow(-1, n - 1) * pow(x, n) / n;
+}
+
 int main() {
     double x;
     double e;
@@ -12,15 +16,15 @@ int main() {
     if (Chek_enter() == false || abs(x) >= 1) { return 0; }
     cout << "Enter eps" << endl;
     cin >> e;
-    if (e <= 0 || e >= 1|| Chek_enter() == false) { return 0; }
+    if (e <= 0 || e >= 1 || Chek_enter() == false) { return 0; }
 
     double curr = x;
     double _sum = curr;
-    for (int n = 2; abs(curr) > e; n++) {
-        curr = pow(-1, n-1) * pow(x, n) / n;
+    for (double n = 2; abs(Res(x, n - 1) - Res(x, n)) > e; n++) {
+        curr = Res(curr, n);
         _sum += curr;
     }
-    
+
     cout << _sum << endl;
     return 0;
 }
