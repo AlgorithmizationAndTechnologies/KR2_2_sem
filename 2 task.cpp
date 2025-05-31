e13df4552dfdceb0f9c0d794d3892c2309ac190c
M1roll <gludara104@gmail.com>
2025-05-31 17:50:51 +1000

    Update 2 task.cpp

--- 2 task.cpp
+++ 2 task.cpp
@@ -3,23 +3,24 @@
 #include "1 task Area.h"
 
 using namespace std;
+
 int main() {
     double x;
     double e;
     cout << "Enter x" << endl;
     cin >> x;
-    if (Chek_enter() == false || abs(x) < 1) { return 0; }
+    if (Chek_enter() == false || abs(x) >= 1) { return 0; }
     cout << "Enter eps" << endl;
     cin >> e;
-    if (abs(e) > 1 || Chek_enter() == false){return 0;}
+    if (e <= 0 || e >= 1|| Chek_enter() == false) { return 0; }
 
-    double _sum = 0;
     double curr = x;
-    for (int i = 2; abs(curr) < e; i++) {
-        curr = (-1) * x * (i - 1) / i;
+    double _sum = curr;
+    for (int n = 2; abs(curr) > e; n++) {
+        curr = pow(-1, n-1) * pow(x, n) / n;
         _sum += curr;
     }
+    
     cout << _sum << endl;
     return 0;
 }
-
