045aa510a08333655db1143c92a3b55f422e4784
M1roll <gludara104@gmail.com>
2025-05-31 19:50:39 +1000

    Update 3 Task.cpp

--- 3 Task.cpp
+++ 3 Task.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 string Bit_stat(unsigned short x, unsigned int n) {
-    unsigned short mask = 1 << n - 1;
+    unsigned short mask = 1 << n;
     if ((x & mask) == 0)
         return "Off";
     else
